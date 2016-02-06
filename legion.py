c159df5e7e488faab9fecb7db8179bcff37b6016
Jordan Ribera <jribera@rover.com>
2016-02-05 22:33:24 -0800

    targets

--- legion.py
+++ legion.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 import sys
-
+import json
+import shutil
+from pprint import pprint
 
 class Legion():
     default_mode = 'help'
     real_modes = ('help', 'capture', 'invade')
+
+    def __init__(self, targets):
+        self.targets = targets
 
     def help(self):
         print('Please specify a mode.')
@@ -15,6 +20,11 @@ class Legion():
     def capture(self):
         print('Capturing...')
         # do stuff
+        for this_target in self.targets.keys():
+            print('    capturing {}'.format(this_target))
+            home = self.targets[this_target]['home']
+            cell = self.targets[this_target]['cell']
+            print('{} >> {}'.format(home, cell))
         print('...done.')
 
     def invade(self):
@@ -23,7 +33,10 @@ class Legion():
         print('...done.')
 
 if __name__ == "__main__":
-    legion = Legion()
+    with open('targets.json') as targets_file:
+        targets = json.load(targets_file)
+
+    legion = Legion(targets)
     mode = legion.default_mode
     if len(sys.argv) > 1:
         if sys.argv[1] in legion.real_modes:
